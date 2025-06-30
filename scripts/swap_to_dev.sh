313fe29ecbafd809b42cbae0102ec8d769b103ef
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-06-30 14:32:56 +0100

    .sh for swapping temporarily over to dev env

---
+++ scripts/swap_to_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DEV_SH="$HOME/bigbluebutton/bigbluebutton-html5/run-dev.sh"
+PROD_SH="$HOME/bigbluebutton/bigbluebutton-html5/run-prod.sh"
+
+prod() {
+	if [ -f "$PROD_SH" ]; then
+		$PROD_SH
+	else
+		echo "PROD_SH not found: $PROD_SH"
+	fi
+	exit 0
+}
+
+dev() {
+	if [ -f "$DEV_SH" ]; then
+		$DEV_SH
+	else
+		echo "DEV_SH not found: $DEV_SH"
+	fi
+}
+
+dev()	# Runs bigbluebutton-html5 from bigbluebutton/dev/bigbluebutton-html5.
+prod()	# Swaps back.
