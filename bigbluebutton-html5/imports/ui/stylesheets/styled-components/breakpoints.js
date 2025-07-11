5083ebfd77197fd25c3933cd366e96df4b53f22c
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-07-11 19:25:03 +0100

    hacky solution to preventing mobile ui on tiny screens

--- bigbluebutton-html5/imports/ui/stylesheets/styled-components/breakpoints.js
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/breakpoints.js
@@ -1,4 +1,4 @@
-const smallOnly = 'only screen and (max-width: 40em)';
+const smallOnly = 'only screen and (max-width: 1px)'; // This is a hack to prevent the mobile UI from kicking in.
 const mediumOnly = 'only screen and (min-width: 40.063em) and (max-width: 64em)';
 const mediumUp = 'only screen and (min-width: 40.063em)';
 const mediumDown = 'only screen and (max-width: 40.0629em)';
